51e0838360747db41501c54fce7f3e676a89bdb9
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-11-06 02:10:36 +0530

    Updated index.js component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './app';
+
+import './style.css';
+
+//let element = React.createElement('div',null,'Hello World');
+
+/*let element = React.createElement(
+    'div',
+    null,
+    React.createElement('h1',
+    null,
+    React.createElement('span',null,'Hello World')
+    )
+    );*/
+
+    // Using JSX we can solve the problem of writing multiple times the same React.createElement()
+
+    //let element = <div><h1><span>Hello World</span></h1></div>;
+
+    // Behind the scenes JSX performs operations of building React Component by creating Virtual DOMs and pushing them into the React DOM internally each and every time when it collides wwith each and every HTML tags.
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
+//React.render(element,document.getElementById('#root'));
+
+
+
+
+
+
