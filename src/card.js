72212b4d4272aeff378b19c0806b3444a5fbbc39
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-11-06 02:09:40 +0530

    Updated card.js file of this repo

--- src/card.js
+++ src/card.js
@@ -4,9 +4,9 @@ class Card extends Component {
     state = {  }
     render() { 
         return ( 
-            <div>
-                <img src=""></img>
-                <div>
+            <div className='card'>
+                <img alt="" src="https://media.istockphoto.com/photos/dangerous-hooded-hacker-breaks-into-government-data-servers-and-a-picture-id817486228?k=6&m=817486228&s=612x612&w=0&h=pZd6esBWGq7TVUuUhLyJziNnhK9UPRhOO2cpU7JDTMg="></img>
+                <div className='blue-text'>
                     Card #1
                 </div>
             </div>
