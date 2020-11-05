d5a7832ff4508804df4b80e8f386fae88d3d606c
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-11-05 23:57:14 +0530

    Contructed Cards for each items of the page

---
+++ src/card.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <img src=""></img>
+                <div>
+                    Card #1
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Card;
