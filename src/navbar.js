7ab1fd4295f899bdfa72253b5e4c5d65f6629c8e
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-11-07 00:15:26 +0530

    Modified navbar for index page

--- src/navbar.js
+++ src/navbar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
+/*class Navbar extends Component {
     state = {  }
     render() { 
         return ( 
         <div id='navbar'>Hello World</div>
+        );
+    }
+}*/
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <nav className="navbar bg-danger">
+            <a href="#" className="navbar-brand text-white">Flipkart</a>
+        </nav>
         );
     }
 }
