aa0fcf4b5a738f24b577cf5f440ed59e345754d7
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-11-07 00:14:54 +0530

    Updated card.js file for project illustration

--- src/card.js
+++ src/card.js
@@ -4,10 +4,12 @@ class Card extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className='card'>
-                <img alt="" src="https://media.istockphoto.com/photos/dangerous-hooded-hacker-breaks-into-government-data-servers-and-a-picture-id817486228?k=6&m=817486228&s=612x612&w=0&h=pZd6esBWGq7TVUuUhLyJziNnhK9UPRhOO2cpU7JDTMg="></img>
-                <div className='blue-text'>
-                    Card #1
+            <div class="card" style={{width: 300,height: 450}}>
+                <img class="card-img-top" src={this.props.pic} style={{height: 200}} alt="Card image cap"></img>
+                <div class="card-body">
+                    <h5 class="card-title">{this.props.title}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
          );
